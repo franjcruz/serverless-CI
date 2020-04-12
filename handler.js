747b639cf07164757af49c6064a7c92a3a6ffc2f
Fran Cruz <franjcruzj@gmail.com>
2020-04-12 15:44:59 +0200

    Testing CI

--- handler.js
+++ handler.js
@@ -1,11 +1,11 @@
 'use strict';
 
-module.exports.hello = async event => {
+module.exports.hello = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Hello World!',
         input: event,
       },
       null,
